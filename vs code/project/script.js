ff71add18e55ed2331e9d215ef70b0fdee4dfed3
Ujjwal patle <136487317+Ujjwalpatle@users.noreply.github.com>
2023-11-10 13:23:25 +0530

    ujjwal

--- vs code/project/script.js
+++ vs code/project/script.js
@@ -1,35 +1,109 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const slides = document.querySelectorAll(".slide");
-    const prevArrow = document.querySelector(".arrow.prev");
-    const nextArrow = document.querySelector(".arrow.next");
-    let currentIndex = 0;
 
-    function showSlide(index) {
-        slides.forEach((slide, i) => {
-            if (i === index) {
-                slide.style.display = "block";
-            } else {
-                slide.style.display = "none";
+    let slideIndex = 0;
+    let slideshowActive = true; // Flag to track slideshow activity
+    let slideshowTimeout;
+
+    // Add event listener to make image fullscreen
+    const images = document.querySelectorAll('.mySlides img');
+    images.forEach(image => {
+        image.addEventListener('click', toggleFullscreen);
+    });
+
+    // Manual control buttons
+    const prevButton = document.querySelector('.prev');
+    const nextButton = document.querySelector('.next');
+
+    prevButton.addEventListener('click', () => plusDivs(-1));
+    nextButton.addEventListener('click', () => plusDivs(1));
+
+    function toggleFullscreen() {
+        const image = this;
+        const isFullscreen = image.classList.toggle('fullscreen');
+        document.body.style.overflow = isFullscreen ? 'hidden' : 'auto';
+
+        // Pause or resume the slideshow based on fullscreen mode
+        slideshowActive = !isFullscreen;
+        if (slideshowActive) {
+            showSlides();
+        } else {
+            clearTimeout(slideshowTimeout);
+        }
+    }
+
+    // Exit fullscreen when clicking outside the image
+    document.addEventListener('click', exitFullscreen);
+
+    function exitFullscreen(event) {
+        const fullscreenImage = document.querySelector('.fullscreen');
+        if (fullscreenImage && !fullscreenImage.contains(event.target)) {
+            fullscreenImage.classList.remove('fullscreen');
+            document.body.style.overflow = 'auto';
+            slideshowActive = true;
+            showSlides(); // Resume slideshow after exiting fullscreen
+        }
+    }
+
+    // Manual control functions
+    function plusDivs(n) {
+        clearTimeout(slideshowTimeout);
+        showDivs((slideIndex -= n));
+        // Start automatic slideshow after manual control
+        slideshowTimeout = setTimeout(showSlides, 2000);
+    }
+
+    function showDivs(n) {
+        let i;
+        let slides = document.getElementsByClassName("mySlides");
+        let dots = document.getElementsByClassName("dot");
+
+        if (n > slides.length) {
+            slideIndex = 1;
+        }
+        if (n < 1) {
+            slideIndex = slides.length;
+        }
+
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+        }
+
+        slides[slideIndex - 1].style.display = "block";
+        dots[slideIndex - 1].className += " active";
+    }
+
+    // Automatic slideshow
+    function showSlides() {
+        if (slideshowActive) {
+            let i;
+            let slides = document.getElementsByClassName("mySlides");
+            let dots = document.getElementsByClassName("dot");
+
+            for (i = 0; i < slides.length; i++) {
+                slides[i].style.display = "none";
             }
-        });
+
+            slideIndex++;
+
+            if (slideIndex > slides.length) {
+                slideIndex = 1;
+            }
+
+            for (i = 0; i < dots.length; i++) {
+                dots[i].className = dots[i].className.replace(" active", "");
+            }
+
+            slides[slideIndex - 1].style.display = "block";
+            dots[slideIndex - 1].className += " active";
+
+            // Reset the timeout for automatic slideshow
+            slideshowTimeout = setTimeout(showSlides, 2000);
+        }
     }
 
-    function prevSlide() {
-        currentIndex = (currentIndex - 1 + slides.length) % slides.length;
-        showSlide(currentIndex);
-    }
+    // Initial call to start the slideshow
+    showSlides();
 
-    function nextSlide() {
-        currentIndex = (currentIndex + 1) % slides.length;
-        showSlide(currentIndex);
-    }
-
-    prevArrow.addEventListener("click", prevSlide);
-    nextArrow.addEventListener("click", nextSlide);
-
-    // Automatic slide change every 3 seconds
-    setInterval(nextSlide, 3000);
-
-    // Initially show the first slide
-    showSlide(currentIndex);
-});
