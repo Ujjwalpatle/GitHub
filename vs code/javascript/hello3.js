94699eb3947df33cf6fa55af35e537162f368e1a
Ujjwal patle <136487317+Ujjwalpatle@users.noreply.github.com>
2024-01-05 20:47:35 +0530

    ujjwal

---
+++ vs code/javascript/hello3.js
@@ -0,0 +1,12 @@
+/*console.log("ujjwal Patel");
+alert("hello");*/
+
+x=null;
+y=undefined;
+name='ujjwalpatle';
+stander = 12;
+console.log('name',name);
+console.log("class",stander);
+console.log('age',x);
+console.log('gender',y);
+console.log('gender',y);
